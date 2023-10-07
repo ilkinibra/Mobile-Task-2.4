3cfdf3146fcfe276ed2da293a2c91b2346404d6a
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-07 22:22:23 +0400

    Add files via upload

---
+++ Task 2.4/index.js
@@ -0,0 +1,15 @@
+function sademurekkeb(eded){
+if(eded <=1){
+    return 'ne sade eded nede murekkeb deyil';
+}
+for(let i= 2; i<= Math.sqrt(eded);i++){
+    if(eded%i===0){
+        return 'murekkeb eded'
+    }
+
+}
+return 'sade eded'
+}
+const eded=31;
+const status =sademurekkeb(eded);
+console.log(status);
